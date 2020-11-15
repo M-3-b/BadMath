b664e08ca83c23392607a80691a01aa5fd15080b
meb_ <meb@meb.meb>
2020-11-15 18:44:56 -0500

    added parentheses to average, and let n3 be defined

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;// only i3 was being divided not all added together
 }
 
 int main()
@@ -13,11 +13,11 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;//redefining n2 while leaving n3 blank
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
-
+	cout << setprecision(2) << fixed << "The average is " << a << endl;
+	//personal preference to have the set precision at 2
 	return 0;
 }
